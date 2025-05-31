55f199821b93c4897ca057cdfacceeaa2a0080f4
p11 <github@programmersunion.in>
2025-05-31 12:46:37 +0530

    updatedd

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e3200cba8d644650")
+/******/ 		__webpack_require__.h = () => ("e452a28175fbb3b5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,87 +1,83 @@
-"use client"
-import React, { useState, useEffect } from 'react';
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { FaShoppingBag } from 'react-icons/fa';
+import { BsChevronDown } from 'react-icons/bs';
 import Image from 'next/image';
 
-const Header: React.FC = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [showSubmenu, setShowSubmenu] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 20) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const toggleSubmenu = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setShowSubmenu(!showSubmenu);
-  };
+export default function Header() {
+  const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <header className={ ` header ${isScrolled ? 'scrolled' : ''}`}>
-      <div className="">
-        <div className="nav-wrapper">
-          {/* Logo */}
+    <header className="navbar navbar-expand-lg  py-3 ">
+      <div className="container  d-flex justify-content-between align-items-center">
+        {/* Logo */}
           <div className="logo-container">
             <Link href="/">
               <Image src="https://cdn.prod.website-files.com/67c0036d59183921f0311a84/67c00d8c1e9d19226f05db51_navbar-logo.svg" alt="WavZai Logo" width={100} height={100} className="logo" />
             </Link>
           </div>
-          
-          {/* Navigation */}
-          <nav className="main-navigation">
-            <ul className="nav-list">
-              <li className="nav-item">
-                <Link href="/" className="nav-link">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/about" className="nav-link">About Us</Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/menu" className="nav-link">Menu</Link>
-              </li>
-              <li className="nav-item has-submenu">
-                <Link href="#" className="nav-link" onClick={toggleSubmenu}>
-                  Pages
-                </Link>
-                {showSubmenu && (
-                  <ul className="submenu">
-                    <li className="submenu-item">
-                      <Link href="/services" className="submenu-link">Services</Link>
-                    </li>
-                    <li className="submenu-item">
-                      <Link href="/gallery" className="submenu-link">Gallery</Link>
-                    </li>
-                    <li className="submenu-item">
-                      <Link href="/testimonials" className="submenu-link">Testimonials</Link>
-                    </li>
-                  </ul>
-                )}
-              </li>
-              <li className="nav-item">
-                <Link href="/blogs" className="nav-link">Blogs</Link>
-              </li>
-            </ul>
-          </nav>
-          
-          {/* Book A Table Button */}
+
+        {/* Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setNavOpen(!navOpen)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Nav */}
+        <div className={`collapse navbar-collapse ${navOpen ? 'show' : ''}`}>
+          <ul className="navbar-nav ms-auto align-items-center gap-lg-4">
+            <li className="nav-item">
+              <Link href="/" className="nav-link active text-info fw-semibold">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about" className="nav-link fw-semibold">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/menu" className="nav-link fw-semibold">
+                Menu
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                href="#"
+                className="nav-link fw-semibold d-flex align-items-center"
+              >
+                Pages <BsChevronDown className="ms-1" size={12} />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/blogs" className="nav-link fw-semibold">
+                Blogs
+              </Link>
+            </li>
+
+            {/* Shopping icon with badge */}
+            <li className="nav-item position-relative">
+              <button className="btn icon-btn">
+                <FaShoppingBag className="text-primary" />
+               
+              </button>
+            </li>
+
+            {/* Book a Table Button */}
           <div className="action-button">
             <Link href="/booking" className="book-table-btn">
               <span>BOOK A TABLE</span>
             </Link>
           </div>
+          </ul>
         </div>
       </div>
     </header>
   );
-};
-
-export default Header;
+}
